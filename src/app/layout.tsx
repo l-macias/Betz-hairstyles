6261264c0e7a65c85d7e06bcae91342a9a93ce2d
l-macias <chueko@gmail.com>
2026-05-11 17:43:26 -0300

    GA4 tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -94,6 +94,23 @@ export default function RootLayout({
   return (
     <html lang="es-AR" className={`${gilda.variable} ${jost.variable}`}>
       <body>
+        {/* --- GOOGLE ANALYTICS 4 (Optimizado) --- */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-0XBJ37DFC7`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-0XBJ37DFC7');
+            `,
+          }}
+        />
         <Script
           id="schema-hair-salon"
           type="application/ld+json"
